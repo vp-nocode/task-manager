09fd63deb956bbf441e8a41ef5f37473d485442f
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-04-20 19:33:28 +0500

    add class TaskManager

--- main.py
+++ main.py
@@ -5,21 +5,42 @@ class Task:
         self.deadline = deadline
         self.status = False
 
-    def add_task(self):
-        pass
-
     def mark_task(self):
         self.status = True
-
-    def list_tasks(self):
-        pass
 
     def __str__(self):
         desc = f"Описание: {self.description}, Срок: {self.deadline}, {'Выполнено' if self.status else 'Не выполнено'}"
         return desc
 
 
-task1 = Task("Написать программу", "2024-04-20")
-task2 = Task("Пройти курс по Python", "2024-07-31")
-print(task1.__str__())
-print(task2.__str__())
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def mark_task_completed(self, description):
+        for task in self.tasks:
+            if task.description == description:
+                task.mark_task()
+                print(f"Задача '{description}' отмечена как выполненная.")
+                return
+        print(f"Задача '{description}' не найдена.")
+
+    def show_current_tasks(self):
+        print("Текущие задачи:")
+        for task in self.tasks:
+            if not task.status:
+                print(task)
+
+
+task_manager = TaskManager()
+task_manager.add_task(Task("Написать программу", "2024-04-20"))
+task_manager.add_task(Task("Пройти курс по Python", "2024-07-31"))
+
+task_manager.show_current_tasks()
+
+task_manager.mark_task_completed("Написать программу")
+task_manager.show_current_tasks()
+
